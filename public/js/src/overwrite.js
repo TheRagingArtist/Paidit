06c062def6b8954c1f8255549bda2b5305875f25
Shannon <shannon.dehart.17@cnu.edu>
2019-03-23 04:23:20 -0400

    Trying new Template

--- public/js/src/overwrite.js
+++
@@ -1,9 +0,0 @@
-/**
- * File: overwrite.js
- * Type: Javascript Component Overwrite
- * Author: Chris Humboldt
- */
-
-if (typeof $loaderplateDefault !== 'undefined') {
-	$loaderplateDefault.path = web.url('baseUrl') + '/webplate/project/component/loaderplate/loaders/';
-}
